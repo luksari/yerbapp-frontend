d3c7f4c1ac3cb515e582262a4460033da566b6d8
Łukasz Tyszkiewicz <lukasz.tyszkiewicz@leocode.com>
2019-11-02 20:36:44 +0100

    Workind on config with emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+import { darken } from 'polished';
 
-const App: React.FC = () => {
+const StyledDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: ${darken(0.2, 'palevioletred')};
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 10rem;
+`;
+
+const App: FC = () => {
   const name = 'YerbApp';
-  return <div className="App">{name}</div>;
+  return (
+    // eslint-disable-next-line jsx-a11y/accessible-emoji
+    <StyledDiv className="App">
+      🌿
+      {name}
+      🌿
+    </StyledDiv>
+  );
 };
 
 export default App;
