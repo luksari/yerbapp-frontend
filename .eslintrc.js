58fc92289eb7ea870d9a47a1463a7716c8365431
luksari <luksari23@gmail.com>
2019-11-16 17:17:39 +0100

    YAPP-03 modified rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,9 +42,9 @@ module.exports = {
     'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-filename-extension': 0,
     'react/jsx-no-target-blank': 0,
-    'react/require-default-props': 0,
     'react/require-extension': 0,
     'react/self-closing-comp': 0,
     'react/sort-comp': 0,
+    'import/prefer-default-export': 0,
   },
 };
