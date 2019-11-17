1ed3bf6ebbc6c7df0ce471bf8d89418c75d0497a
Łukasz Tyszkiewicz <lukasz.tyszkiewicz@leocode.com>
2019-11-17 16:03:14 +0100

    Attempt to configure Redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,35 @@ import './index.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import history from './utils/history';
 import * as serviceWorker from './serviceWorker';
+
 import App from './App';
+import configureStore from './store/configureStore';
 
 /** @todo Pass proper api URI */
 const client = new ApolloClient({
   uri: '',
 });
 
+// Create redux store with history
+const initialState = {};
+const store = configureStore(initialState, history);
+const MOUNT_NODE = document.getElementById('root');
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <ApolloHooksProvider client={client}>
-      <App />
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </Provider>
     </ApolloHooksProvider>
   </ApolloProvider>,
-  document.getElementById('root'),
+  MOUNT_NODE,
 );
 
 // If you want your app to work offline and load faster, you can change
