83e19f5d64af27a6438c4943913baa4276f64187
Łukasz Tyszkiewicz <lukasz.tyszkiewicz@leocode.com>
2019-11-18 19:49:27 +0100

    Added proper uris to graphql provider

---
+++ src/config/uri.ts
@@ -0,0 +1,4 @@
+export const SERVER_URI = 'http://157.230.108.68:3000/';
+export const GRAPHQL_URI = `${SERVER_URI}graphql`;
+export const API_URI = `${SERVER_URI}api`;
+export const AUTH_URI = `${SERVER_URI}auth`;
