75e1b784c055fac1587458d51f0fcd99a7d1826c
luksari <luksari23@gmail.com>
2019-11-19 18:54:50 +0100

    Working on saga injector

--- src/store/types.ts
+++ src/store/types.ts
@@ -2,9 +2,11 @@ import {
   Store, Action, AnyAction, Reducer,
 } from 'redux';
 import { RouterState } from 'connected-react-router';
+import { Saga } from 'redux-saga';
 
 export interface AppStore extends Store<{}> {
   injectedReducers: Record<string, Reducer<any, AnyAction>>;
+  injectedSagas: Record<string, any>;
 }
 
 export interface InjectReducerParams {
@@ -16,4 +18,21 @@ export interface ReduxState {
   route: RouterState;
   global: object;
   [propName: string]: any;
+}
+
+export enum SagaMode {
+  RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount',
+  DAEMON = '@@saga-injector/daemon',
+  ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount',
+}
+
+export interface SagaConfig {
+  key: string;
+  saga: Function;
+  mode?: SagaMode;
+}
+
+export interface DescriptorModel {
+  saga: Function;
+  mode?: SagaMode;
 }
