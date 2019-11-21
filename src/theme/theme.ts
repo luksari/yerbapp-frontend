b5a1098647e13c5fb9f87f35edf5fffc93854e1a
luksari <luksari23@gmail.com>
2019-11-21 20:33:06 +0100

    Added sign up form :boom:

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,13 +1,30 @@
 export const theme = {
   colors: {
-
+    mainText: '#0F1A20',
+    alternativeText: '#FFFFFF',
+    labelText: '#424B54',
+    disabledText: '#93A8AC',
+    disabledBackground: '#EDF4ED',
+    danger: '#F42C04',
+    warning: '#FF9F1C',
+    decorators: '#E8E9F3 ',
+    primary: '#9BC53D',
+    alternative: '#2E933C',
   },
   metrics: {
     fontSize: {
-
+      xs: '0.7rem',
+      s: '0.9rem',
+      m: '1rem',
+      l: '1.8rem',
+      xl: '2.7rem',
+      xxl: '4rem',
     },
     margin: {
-
+      s: '5px',
+      m: '15px',
+      l: '40px',
+      xl: '45px',
     },
     padding: {
 
