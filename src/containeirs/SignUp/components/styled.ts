73b58de67a9f7df81a54f01553bb1103bf58b3fc
luksari <luksari23@gmail.com>
2019-11-23 14:54:03 +0100

    Styled components and theme

---
+++ src/containeirs/SignUp/components/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'theme/theme';
+
+/** @see Example usage of styled component styling */
+export const StyledForm = styled('form')`
+  background: ${({ theme }) => theme.colors.mainBackground}
+
+`;
