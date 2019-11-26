13b210762d8694ec2baa321786135c9b4115495a
Łukasz <luk.jag@o2.pl>
2019-11-26 11:28:32 +0100

    Created initial Navbar

---
+++ src/components/Routes/Tutorial.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class TutorialPage extends Component {
+    render() {
+        return <div>The Tutorial Page</div>
+    }
+}
+
+export default TutorialPage;
