0a401eddfaeddf7cd5579f1c4d67463e3a6182f2
Łukasz <luk.jag@o2.pl>
2019-11-27 13:59:11 +0100

    Navbar improve & first pages

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,20 +1,102 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
-import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { styled } from 'theme/theme';
+
+const Wrapper = styled('div')`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    font-family: 'Josefin Sans', sans-serif;
+  }
+
+  .nav-wrapper {
+    display: flex;
+    position: center;
+  }
+
+  .nav-wrapper .nav-sidebar {
+    width: 20%;
+    float:left;
+    height: 100%;
+    background: #333340;
+    padding: 30px 0px;
+    position: fixed;
+  }
+
+  .nav-wrapper .nav-sidebar h2{
+    color: #fff;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 20px;
+    font-size: 30px;
+  }
+
+  .nav-wrapper .nav-sidebar ul li{
+    border-bottom: 1px solid rgba(0,0,0,0.05);
+    border-top: 1px solid rgba(255,255,255,0.05);
+    text-align: center;
+    font-size: 25px;
+  }
+
+  .nav-wrapper .nav-sidebar ul li:hover{
+    background-color: #594f8d;
+  }
+
+  .nav-wrapper .nav-sidebar ul li a{
+    color: #bdb8d7;
+    display: block;
+    padding: 15px;
+  }
+
+  .wrapper .sidebar ul li:hover a{
+    color: #fff;
+  }
+`;
 
 const Navbar = () => {
     return (
-    <nav className="nav-wrapper">
-        <div className="container">
-            <ul className="nav-links">
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/tutorial'>Tutorial</Link></li>
-                <li><Link to='/explore'>Explore</Link></li>
-                <li><Link to='/signup'>Signup</Link></li>
-                <li><Link to='/signin'>Login</Link></li>
-            </ul>
-        </div>
-    </nav>
+    <Wrapper>
+        <nav className="nav-wrapper">
+            <div className="nav-sidebar">
+                <h2>YerbaApp</h2>
+                <ul>
+                    <li>
+                        <Link to='/'>
+                        <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/tutorial'>
+                        <a>Tutorial</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/explore'>
+                        <a>Explore</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>
+                        <a>About</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/profile'>
+                        <a>Profile</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>
+                        <a>Contact</a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </Wrapper>
     )
 }
 
---
+++ src/containeirs/Tutorial/Tutorial.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+import { styled } from 'theme/theme';
+
+const Wrapper = styled('div')`
+  width: 80%;
+  float:right;
+  height: 100vh;
+  background: ${({ theme }) => theme.colors.mainBackground};
+  display: flex;
+  justify-content: center;
+  margin: ${({ theme }) => theme.metrics.margin.s};
+  font-size: ${({ theme }) => theme.metrics.fontSize.m};
+
+  h3 {
+    background: ${({ theme }) => theme.colors.alternative};
+    font-size: ${({ theme }) => theme.metrics.fontSize.xl};
+  }
+`;
+
+const Tutorial: FC = () => {
+    return (
+      <Wrapper>
+        <div className="container">
+          <h3 className="center">How to start?</h3>
+          <p>Do you want to know how to prepare your first Yerba Mate? This tutorial is perfect for you!</p>
+        </div>
+      </Wrapper>
+    )
+  }
+  
+  export default Tutorial
