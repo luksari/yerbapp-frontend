f2117b7a45d0bb685afc8267fb74f93b268566b2
Mateusz Franik <mfranik@emag.local>
2019-11-28 14:46:13 +0100

    About structure changed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 import { Login } from 'containeirs/Login';
 import { Layout } from 'containeirs/Layout';
 import { UserRole } from 'models/user';
+import About from 'containeirs/About/About';
+
 
 const Wrapper = styled('div')`
   width: 100vw;
@@ -22,6 +24,7 @@ const App: FC = () => {
         <Route exact path="/" render={() => <div>Homepage, at least some kind of...</div>} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/about" component={About} />
       </Switch>
     </Wrapper>
   );
