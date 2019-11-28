fe14fa53fef872082ee80b34e638b641188782cc
luksari <luksari23@gmail.com>
2019-11-28 12:08:02 +0100

    Working on Authenticated route

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { StyledFooter } from './styled';
+
+export const Footer: FC = () => (
+  <StyledFooter>
+    To jest przykładowy footer!
+  </StyledFooter>
+);
