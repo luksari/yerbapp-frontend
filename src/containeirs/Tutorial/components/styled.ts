60ff4dbc9e8ee59fc312c3f43176a68fe244d3d8
M F <matif96@o2.pl>
2019-11-30 19:47:47 +0100

    Tutorial very initial structure

---
+++ src/containeirs/Tutorial/components/styled.ts
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components';
+/** @see Example usage of styled component styling */
+export const TutorialContainer = styled.section`
+  ${({ theme }) => css`
+      background: ${theme.colors.mainBackground};
+      padding: ${theme.metrics.padding.m};
+      font-size: ${theme.metrics.fontSize.m};
+      display: grid;
+  `}
+`;
