1108eb81ea719f0215ed780b5fa7ac7876f5c2aa
M F <matif96@o2.pl>
2019-12-01 20:37:45 +0100

    Fixing Tutorial

--- src/containeirs/About/Loadable.tsx
+++ src/containeirs/About/Loadable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import loadable from 'utils/Loadable';
 
-export const Login = loadable(() => import('./About'), {
+export const About = loadable(() => import('./About'), {
   fallback: <div>loading...</div>,
 });
