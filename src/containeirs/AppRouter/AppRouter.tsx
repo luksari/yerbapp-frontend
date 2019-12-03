c929aeb63a038193bf86612a305fbe351e22f9ec
Łukasz Tyszkiewicz <lukasz.tyszkiewicz@leocode.com>
2019-12-03 20:57:34 +0100

    Fix in Tutorial

--- src/containeirs/AppRouter/AppRouter.tsx
+++ src/containeirs/AppRouter/AppRouter.tsx
@@ -7,12 +7,16 @@ import { AuthLayout } from 'components/Layouts/AuthLayout';
 import { AuthenticatedRoute, LayoutRoute } from 'components/Routes';
 import { Tutorial } from 'containeirs/Tutorial';
 import { About } from 'containeirs/About';
+import { Home } from 'containeirs/Home';
+import { Explore } from 'containeirs/Explore';
 
 export const AppRouter: FC = () => {
   return (
     <Switch>
+      <AuthenticatedRoute exact path="/" component={Home} layout={AppLayout} canBeGuest />
       <AuthenticatedRoute exact path="/tutorial" component={Tutorial} layout={AppLayout} canBeGuest />
       <AuthenticatedRoute exact path="/about" component={About} layout={AppLayout} canBeGuest />
+      <AuthenticatedRoute exact path="/explore" component={Explore} layout={AppLayout} canBeGuest />
       <LayoutRoute exact path="/login" component={Login} layout={AuthLayout} />
       <LayoutRoute exact path="/register" component={Register} layout={AuthLayout} />
     </Switch>
