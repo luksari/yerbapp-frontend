71c816c074e7cc7a15b4a96b45021cda354f7bb4
luksari <luksari23@gmail.com>
2019-12-08 11:53:41 +0100

    Changed queries

--- src/components/Topbar/styled.ts
+++ src/components/Topbar/styled.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const StyledTopbar = styled.section`
   box-shadow: 0px 10px 10px ${({ theme }) => theme.colors.decorators};
@@ -8,4 +9,19 @@ export const StyledTopbar = styled.section`
   justify-content: flex-end;
   align-items: center;
   padding: ${({ theme }) => theme.metrics.padding.xl};
+`;
+
+export const UserProfileLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const UserTitle = styled.p`
+  ${({ theme }) => css`
+    font-size: ${theme.metrics.fontSize.s};
+    color: ${theme.colors.labelText};
+    margin: 0 ${theme.metrics.margin.xl};
+    font-weight: 700;
+  `}
 `;
--- src/queries/UserQueries.ts
+++ src/queries/UserQueries.ts
@@ -3,7 +3,26 @@ import { gql } from 'apollo-boost';
 export const GET_USER = gql`
   query getUser($userId:  ID!) {
     user(userId: $userId) {
-      username
+      username,
+      email,
+    }
+  }
+`;
+
+export const GET_ME = gql`
+  query getMe {
+    whoAmI{
+      username,
+      email
+    }
+  }
+`;
+
+export const GET_ME_DETAILS = gql`
+  query getMeDetails {
+    whoAmI{
+      username,
+      email
     }
   }
 `;
