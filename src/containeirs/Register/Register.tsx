2069ed57308dfbecb445157c34501fc82788e3a9
Łukasz Tyszkiewicz <lukasz.tyszkiewicz@leocode.com>
2019-12-10 20:11:32 +0100

    working on errors

--- src/containeirs/Register/Register.tsx
+++ src/containeirs/Register/Register.tsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { compose } from '@reduxjs/toolkit';
 import { injectReducer } from 'utils/injectReducer';
 import { injectSaga } from 'utils/injectSaga';
-import { ErrorResponse } from 'utils/types';
 import {
   name, reducer, actions, makeSelectMappedErrors,
 } from './slice';
@@ -15,8 +14,8 @@ import { RegisterFormData } from './types';
 
 interface Props<T = RegisterFormData> {
   setSignUpBegin: (values: T) => void;
-  formErrors: ErrorResponse<T>;
 }
+
 const initialValues: RegisterFormData = {
   username: '',
   email: '',
@@ -24,18 +23,43 @@ const initialValues: RegisterFormData = {
   repeatPassword: '',
 };
 
+const validate = (values: RegisterFormData) => {
+  const errors = {} as Record<keyof RegisterFormData, any>;
+  if (!values.email) {
+    errors.email = 'Pole wymagane';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Nieprawidłowy adres email!';
+  }
+
+  if (!values.password) {
+    errors.password = 'Pole wymagane';
+  } else if (values.password.length < 8) {
+    errors.password = 'Hasło musi mieć minimalnie 8 znaków';
+  }
+
+  if (values.password !== values.repeatPassword) {
+    errors.password = 'Pola nie są takie same';
+    errors.repeatPassword = 'Pola nie są takie same';
+  }
+
+  if (!values.username) {
+    errors.username = 'Pole wymagane';
+  }
+
+  return errors;
+};
+
 
 const RegisterRaw: FC<Props> = ({
   setSignUpBegin,
-  formErrors,
 }) => {
-  console.warn(formErrors);
   return (
     <div>
       <RegisterForm
         title="Utwórz konto"
         initialValues={initialValues}
         onSubmit={(values) => setSignUpBegin(values)}
+        validate={validate}
       >
       </RegisterForm>
     </div>
@@ -45,6 +69,7 @@ const RegisterRaw: FC<Props> = ({
 const mapStateToProps = createStructuredSelector({
   formErrors: makeSelectMappedErrors(),
 });
+
 
 const mapDispatchToProps = (dispatch) => ({
   setSignUpBegin: (payload: RegisterFormData) => dispatch(actions.setRegisterPending(payload)),
