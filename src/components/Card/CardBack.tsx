cf80ac31eee3e721d6ca887413f6a0a0b2f7232a
M F <matif96@o2.pl>
2019-12-11 18:34:12 +0100

    Card with initial style added

--- src/components/Card/CardBack.tsx
+++ src/components/Card/CardBack.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonType } from 'components/Button';
 import {
-  CardContainer, Description,
+  CardContainer, Description, YerbaTitle, CardMain, CardFooter, CardButton,
 } from './styled';
 
 interface CardFrontProps {
@@ -10,9 +12,21 @@ interface CardFrontProps {
 export const CardBack: FC<CardFrontProps> = ({ ChangeSide }) => {
   return (
     <CardContainer onClick={ChangeSide}>
-      <Description>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptatem nam pariatur voluptate perferendis, asperiores aspernatur! Porro similique consequatur, nobis soluta minima, quasi laboriosam hic cupiditate perferendis esse numquam magni.
-      </Description>
+      <YerbaTitle>
+        Pajarito Elaborada
+      </YerbaTitle>
+      <CardMain>
+        <Description>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptatem nam pariatur voluptate perferendis, asperiores aspernatur! Porro similique consequatur, nobis soluta minima, quasi laboriosam hic cupiditate perferendis esse numquam magni.
+        </Description>
+      </CardMain>
+      <CardFooter>
+        <Link to="/explore">
+          <CardButton themeType={ButtonType.Primary}>
+            Dodaj recenzję
+          </CardButton>
+        </Link>
+      </CardFooter>
     </CardContainer>
   );
 };
