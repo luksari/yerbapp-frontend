b132b71882083565552af7f3c3793a7bf4612f5c
M F <matif96@o2.pl>
2019-12-11 18:34:12 +0100

    Card with initial style added

--- src/components/Card/CardFront.tsx
+++ src/components/Card/CardFront.tsx
@@ -12,28 +12,34 @@ interface CardFrontProps {
 export const CardFront: FC<CardFrontProps> = ({ ChangeSide }) => {
   return (
     <CardContainer onClick={ChangeSide}>
-      <YerbaTitle>Producent Nazwa</YerbaTitle>
+      <YerbaTitle>
+        Pajarito Elaborada
+      </YerbaTitle>
       <CardMain>
         <LeftSide>
           <Image src="https://etnobazar.pl/pol_pl_YERBA-MATE-PAJARITO-ELABORADA-250G-BIO-18821_3.jpg" alt="" />
         </LeftSide>
         <RightSide>
           <Details>
-            <li>Pochodzenie: ---</li>
-            <li>Typ yerby: ---</li>
+            <li>Kraj pochodzenia: Paragwaj</li>
+            <li>Typ: Elaborada</li>
           </Details>
           <Rating>
-            <li>Aromat: ---</li>
-            <li>Smak: ---</li>
-            <li>Gorycz: ---</li>
-            <li>Moc: ---</li>
-            <li>Cena: ---</li>
-            <li>Ocena ogólna: ---</li>
+            <li>Aromat: ----|-</li>
+            <li>Smak: ----|-</li>
+            <li>Gorycz: ----|-</li>
+            <li>Moc: ---|--</li>
+            <li>Cena: --|---</li>
+            <li>Ocena ogólna: ----|-</li>
           </Rating>
         </RightSide>
       </CardMain>
       <CardFooter>
-        <Link to="/explore"><CardButton themeType={ButtonType.Primary}>Dodaj recenzję!</CardButton></Link>
+        <Link to="/explore">
+          <CardButton themeType={ButtonType.Primary}>
+            Dodaj recenzję
+          </CardButton>
+        </Link>
       </CardFooter>
     </CardContainer>
   );
