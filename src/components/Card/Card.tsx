fc02d5cf429e08c71bff8ae9e74c336de9c574db
M F <matif96@o2.pl>
2019-12-12 20:16:22 +0100

    Card v2 with props added and style changed

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,37 +1,97 @@
-import React, { Component } from 'react';
-import { CardBack } from './CardBack';
-import { CardFront } from './CardFront';
+import React, { FC } from 'react';
+import { ButtonType } from 'components/Button';
+import { Link } from 'react-router-dom';
+import {
+  CardContainer, YerbaTitle, LeftSide, RightSide, Image, Details, CardButton, Rating, Description,
+} from './styled';
 
 interface CardProps {
-  Product: Array<any>;
+  id: number;
+  manufacturer: string;
+  type: string;
+  name: string;
+  details: string;
+  country: string;
+  photoUrl: string;
+  addedAt: string;
+  aromaScore: number;
+  tasteScore: number;
+  bitternessScore: number;
+  energyScore: number;
+  priceScore: number;
+  overallScore: number;
 }
-
-type CardState = {
-  IsSideFront: boolean;
-}
-
-export class Card extends Component<CardProps, CardState> {
-  constructor(props: CardProps) {
-    super(props);
-
-    this.state = { IsSideFront: true };
-  }
-
-  ChangeSideToBack = () => {
-    this.setState({ IsSideFront: false });
-  }
-
-  ChangeSideToFront = () => {
-    this.setState({ IsSideFront: true });
-  }
-
-  render() {
-    const { IsSideFront } = this.state;
-    return (
-      <div>
-        { IsSideFront ? <CardFront ChangeSide={this.ChangeSideToBack} />
-          : <CardBack ChangeSide={this.ChangeSideToFront} />}
-      </div>
-    );
-  }
-}
+export const Card: FC<CardProps> = ({
+  id, manufacturer, type, name, details, country, photoUrl, addedAt, aromaScore, tasteScore, bitternessScore, energyScore, priceScore, overallScore,
+}) => {
+  return (
+    <CardContainer>
+      <LeftSide>
+        <YerbaTitle>
+          {manufacturer}
+          Pajarito
+          {' '}
+          {name}
+          Elaborada
+        </YerbaTitle>
+        <Image src="https://etnobazar.pl/pol_pl_YERBA-MATE-PAJARITO-ELABORADA-250G-BIO-18821_3.jpg" alt={photoUrl} />
+        <Link to="/explore">
+          <CardButton themeType={ButtonType.Primary}>
+              Zobacz więcej
+          </CardButton>
+        </Link>
+      </LeftSide>
+      <RightSide>
+        <Details>
+          <label>
+              Typ:
+            <p>
+              {type}
+            </p>
+          </label>
+          <label>
+              Kraj pochodzenia:
+            <p>
+              {country}
+            </p>
+          </label>
+        </Details>
+        <Rating>
+          <label>
+            Aromat:
+            {' '}
+            {aromaScore}
+          </label>
+          <label>
+            Smak:
+            {' '}
+            {tasteScore}
+          </label>
+          <label>
+            Gorycz:
+            {' '}
+            {bitternessScore}
+          </label>
+          <label>
+            Moc:
+            {' '}
+            {energyScore}
+          </label>
+          <label>
+            Cena:
+            {' '}
+            {priceScore}
+          </label>
+          <label>
+            Ocena ogólna:
+            {' '}
+            {overallScore}
+          </label>
+        </Rating>
+        <Description>
+          {details}
+        </Description>
+      </RightSide>
+    </CardContainer>
+  );
+};
