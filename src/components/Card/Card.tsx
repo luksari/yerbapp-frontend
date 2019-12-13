dad4a90aa92e4f3feeaf08da84335f99c5fdb94c
Mateusz Franik <mfranik@emag.local>
2019-12-13 13:46:48 +0100

    Card upgraded but not finished yet

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { ButtonType } from 'components/Button';
 import { Link } from 'react-router-dom';
 import {
-  CardContainer, YerbaTitle, LeftSide, RightSide, Image, Details, CardButton, Rating, Description,
+  CardContainer, YerbaTitle, LeftSide, RightSide, Image, CardButton, Description, DataContainer,
 } from './styled';
 
 interface CardProps {
@@ -21,20 +21,33 @@ interface CardProps {
   priceScore: number;
   overallScore: number;
 }
-export const Card: FC<CardProps> = ({
-  id, manufacturer, type, name, details, country, photoUrl, addedAt, aromaScore, tasteScore, bitternessScore, energyScore, priceScore, overallScore,
-}) => {
+
+const exampleData: CardProps[] = [
+  {
+    id: 1,
+    manufacturer: 'Pajarito',
+    type: 'Elaborada',
+    name: 'Pajarito Elaborada',
+    details: 'Pajarito w swojej podstawowej wersji (Elaborada) to produkt bardzo solidny, herbatka brawurowo wręcz pobudza, jest wydajna oraz relatywnie tania. Jej atutem jest również powszechna dostępność. Bardzo dobry wybór dla osób rozpoczynających swoją przygodę z yerba mate.',
+    country: 'Paragwaj',
+    photoUrl: 'https://etnobazar.pl/pol_pl_YERBA-MATE-PAJARITO-ELABORADA-250G-BIO-18821_3.jpg',
+    addedAt: '09.11.2001 21:37:42',
+    aromaScore: 3.3,
+    tasteScore: 4.1,
+    bitternessScore: 3.6,
+    energyScore: 3.9,
+    priceScore: 4.5,
+    overallScore: 4.3,
+  },
+];
+export const Card: FC<CardProps> = () => {
   return (
     <CardContainer>
       <LeftSide>
         <YerbaTitle>
-          {manufacturer}
-          Pajarito
-          {' '}
-          {name}
-          Elaborada
+          {exampleData[0].name}
         </YerbaTitle>
-        <Image src="https://etnobazar.pl/pol_pl_YERBA-MATE-PAJARITO-ELABORADA-250G-BIO-18821_3.jpg" alt={photoUrl} />
+        <Image src={exampleData[0].photoUrl} alt={exampleData[0].name} />
         <Link to="/explore">
           <CardButton themeType={ButtonType.Primary}>
               Zobacz więcej
@@ -42,54 +55,74 @@ export const Card: FC<CardProps> = ({
         </Link>
       </LeftSide>
       <RightSide>
-        <Details>
+        <DataContainer>
           <label>
-              Typ:
-            <p>
-              {type}
-            </p>
+            Producent:
+            {' '}
+            <span>
+              {exampleData[0].manufacturer}
+            </span>
           </label>
           <label>
-              Kraj pochodzenia:
-            <p>
-              {country}
-            </p>
+            Typ:
+            {' '}
+            <span>
+              {exampleData[0].type}
+            </span>
           </label>
-        </Details>
-        <Rating>
+          <label>
+            Kraj pochodzenia:
+            {' '}
+            <span>
+              {exampleData[0].country}
+            </span>
+          </label>
+          <p />
           <label>
             Aromat:
             {' '}
-            {aromaScore}
+            <span>
+              {exampleData[0].aromaScore}
+            </span>
           </label>
           <label>
             Smak:
             {' '}
-            {tasteScore}
+            <span>
+              {exampleData[0].tasteScore}
+            </span>
           </label>
           <label>
             Gorycz:
             {' '}
-            {bitternessScore}
+            <span>
+              {exampleData[0].bitternessScore}
+            </span>
           </label>
           <label>
             Moc:
             {' '}
-            {energyScore}
+            <span>
+              {exampleData[0].energyScore}
+            </span>
           </label>
           <label>
             Cena:
             {' '}
-            {priceScore}
+            <span>
+              {exampleData[0].priceScore}
+            </span>
           </label>
           <label>
             Ocena ogólna:
             {' '}
-            {overallScore}
+            <span>
+              {exampleData[0].overallScore}
+            </span>
           </label>
-        </Rating>
+        </DataContainer>
         <Description>
-          {details}
+          {exampleData[0].details}
         </Description>
       </RightSide>
     </CardContainer>
