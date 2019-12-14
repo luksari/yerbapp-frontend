c14ebd94dfe31056fe20bd227a3aa80fa1789574
luksari <luksari23@gmail.com>
2019-12-14 17:43:37 +0100

    Added manufacturers table, @todo pass proper actions

--- src/containeirs/Manufacturers/Manufacturers.tsx
+++ src/containeirs/Manufacturers/Manufacturers.tsx
@@ -1,19 +1,34 @@
-import React, { memo, FC } from 'react';
+import React, {
+  memo, FC, useState, useEffect,
+} from 'react';
 import { compose } from 'redux';
 import Helmet from 'react-helmet';
 import { Title } from 'components/TitleBar';
 import { useGetManufacturersQuery } from 'generated/graphql';
 import { Loader } from 'components/Loader';
+import { Pagination } from 'components/Pagination';
 import { Wrapper } from './styled';
 import { ManuFacturersTable } from './components/ManufacturersTable';
 
-interface Props {
-  props: any;
-}
+export const ManufacturesRaw: FC = () => {
+  const perPage = 5;
+  const [page, setPage] = useState(1);
+  const [offset, setOffset] = useState(0);
 
+  useEffect(() => {
+    setOffset((prevOffset) => (page * perPage) + prevOffset);
+  }, [page]);
 
-export const ManufacturesRaw: FC<Props> = () => {
-  const { data, loading } = useGetManufacturersQuery();
+  const { data, loading } = useGetManufacturersQuery({ variables: { offset, perPage } });
+
+  const handleEdit = (id: number) => {
+    console.warn(`Redirect to edit form for manufacturer ${id}`);
+  };
+
+  const handleDelete = (id: number) => {
+    console.warn(`Delete  manufacturer ${id}`);
+  };
+
   if (loading) {
     return <Loader fullscreen />;
   }
@@ -21,7 +36,13 @@ export const ManufacturesRaw: FC<Props> = () => {
     <Wrapper>
       <Helmet title="Producenci" />
       <Title>Producenci</Title>
-      <ManuFacturersTable data={data.manufacturers} />
+      <Pagination
+        itemCount={data.manufacturers.length}
+        perPage={perPage}
+        currentPage={1}
+        onPageChange={(value) => setPage(value)}
+      />
+      <ManuFacturersTable data={data.manufacturers} onEdit={handleEdit} onDelete={handleDelete} />
     </Wrapper>
   );
 };
