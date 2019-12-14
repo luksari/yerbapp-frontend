d343181cf912305511e57de5d8446fda57bc86a4
luksari <luksari23@gmail.com>
2019-12-14 20:12:27 +0100

    Fixed rating component

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react';
+import React, {
+  FC, memo, useMemo, useCallback, useState,
+} from 'react';
 import { RatingComponentProps } from 'react-rating';
 import { useFormikContext } from 'formik';
 import { get } from 'lodash';
@@ -12,34 +14,28 @@ type Props = {
   label?: string;
   vertical?: boolean;
   size?: SizeType;
+  onSubmit?: (value: number) => void;
 } & RatingComponentProps;
 
 export const Rating: FC<Props> = ({
   name,
   label,
   initialRating,
-  onChange,
   size = SizeType.Normal,
   vertical = false,
+  onChange,
   ...rest
 }) => {
-  let value;
-  let handleChange;
-
   const context = useFormikContext<any>();
+  const value = context ? get(context.values, name) : initialRating;
+  const [rating, setRating] = useState(value);
+  const handleClick = (val) => setRating(val);
+  const handleChange = useCallback((val: number) => context ? context.setFieldValue(name, val, false) : onChange(val), [rating]);
 
-  if (context) {
-    value = get(context.values, name);
-    handleChange = context.setFieldValue(name, value, false);
-  } else {
-    value = initialRating;
-    handleChange = onChange;
-  }
-
-  return (
+  return useMemo(() => (
     <InputWrapper size={size} vertical={vertical}>
       <InputLabel htmlFor={name}>{label}</InputLabel>
-      <StyledRating {...rest} onChange={handleChange} onClick={handleChange} initialRating={value} emptySymbol={<EmptySymbol />} fullSymbol={<FullSymbol />} quiet />
+      <StyledRating {...rest} onChange={handleChange} onClick={handleClick} initialRating={rating} emptySymbol={<EmptySymbol />} fullSymbol={<FullSymbol />} quiet />
     </InputWrapper>
-  );
+  ), [value]);
 };
