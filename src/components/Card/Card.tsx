a8714d9d6ad0787d359f5cd61937147af7da21d2
M F <matif96@o2.pl>
2019-12-16 19:32:24 +0100

    Card title and margins updated

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import { ButtonType } from 'components/Button';
-import { Rating } from 'components/Rating';
 import { SizeType } from 'components/Rating/types';
 import { truncate } from 'lodash';
 import {
@@ -25,42 +24,8 @@ interface CardProps {
   overallScore: number;
 }
 
-const exampleData: CardProps[] = [
-  {
-    id: 1,
-    manufacturer: 'Pajarito',
-    type: 'Elaborada',
-    name: 'Pajarito Elaborada',
-    details: 'Pajarito w swojej podstawowej wersji (Elaborada) to produkt bardzo solidny, herbatka brawurowo wręcz pobudza, jest wydajna oraz relatywnie tania. Jej atutem jest również powszechna dostępność. Bardzo dobry wybór dla osób rozpoczynających swoją przygodę z yerba mate.',
-    country: 'Paragwaj',
-    photoUrl: 'https://etnobazar.pl/pol_pl_YERBA-MATE-PAJARITO-ELABORADA-250G-BIO-18821_3.jpg',
-    addedAt: '09.11.2001 21:37:42',
-    aromaScore: 3.3,
-    tasteScore: 3.9,
-    bitternessScore: 3.6,
-    energyScore: 4.1,
-    priceScore: 4.5,
-    overallScore: 4.3,
-  },
-];
-
 export const Card: FC<CardProps> = (
-  // {
-  //   name,
-  //   photoUrl,
-  //   manufacturer,
-  //   type,
-  //   country,
-  //   aromaScore,
-  //   tasteScore,
-  //   bitternessScore,
-  //   energyScore,
-  //   priceScore,
-  //   overallScore,
-  //   details,
-  // },
-) => {
-  const {
+  {
     name,
     photoUrl,
     manufacturer,
@@ -73,7 +38,8 @@ export const Card: FC<CardProps> = (
     priceScore,
     overallScore,
     details,
-  } = exampleData[0];
+  },
+) => {
   const shortDescription = truncate(details, { length: 150, separator: '.' });
   return (
     <CardContainer>
@@ -82,10 +48,8 @@ export const Card: FC<CardProps> = (
           {name}
         </YerbaTitle>
         <Image src={photoUrl} alt={name} />
-        <CardButton
-          themeType={ButtonType.Secondary}
-        >
-            Zobacz więcej
+        <CardButton themeType={ButtonType.Secondary}>
+          Zobacz więcej
         </CardButton>
       </DataWrapper>
       <DataWrapper>
