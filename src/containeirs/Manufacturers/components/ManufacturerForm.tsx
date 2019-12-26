5da80c3f7b86c590e8812cfa9e18a37d51929d69
luksari <luksari23@gmail.com>
2019-12-26 13:48:40 +0100

    Refactor for types in profile form

---
+++ src/containeirs/Manufacturers/components/ManufacturerForm.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import { FormProps } from 'utils/types';
+
+
+interface ManufacturerFormData {
+  id: string;
+  name: string;
+  country: string;
+}
+
+const ManufacturerForm: FC<FormProps<any>> = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default ManufacturerForm;
