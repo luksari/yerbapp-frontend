a97709a240d8724b7317cfcb63af540d22e0d27e
luksari <luksari23@gmail.com>
2020-01-04 14:28:19 +0100

    Added separate component for action-bar

---
+++ src/components/ActionBar/ActionBar.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import { Pagination } from 'components/Pagination';
+import { Button, ButtonVariant, ButtonType } from 'components/Button';
+import { Icon } from 'antd';
+import { ActionWrapper } from './styled';
+
+interface Props {
+  total: number;
+  perPage: number;
+  onPageChange: (page: number) => void;
+  redirectCreate: VoidFunction;
+  createText: string;
+}
+
+export const ActionBar: FC<Props> = ({
+  total,
+  perPage,
+  onPageChange,
+  redirectCreate,
+  createText,
+}) => {
+  return (
+    <ActionWrapper>
+      <Pagination
+        itemCount={total}
+        perPage={perPage}
+        currentPage={1}
+        onPageChange={onPageChange}
+      />
+      <Button
+        variant={ButtonVariant.Normal}
+        themeType={ButtonType.Primary}
+        onClick={redirectCreate}
+        icon={<Icon type="plus" />}
+      >
+        {createText}
+      </Button>
+    </ActionWrapper>
+  );
+};
