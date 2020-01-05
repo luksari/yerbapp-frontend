7d92a96b0199bd7292dff9b6a71f35c0dd4b0a1e
Łukasz Tyszkiewicz <lukasz.tyszkiewicz@leocode.com>
2020-01-05 18:11:52 +0100

    Fixed styles in actionBar

--- src/containeirs/Explore/styled.ts
+++ src/containeirs/Explore/styled.ts
@@ -2,5 +2,14 @@ import styled from 'styled-components';
 
 export const RatingWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr)
+  grid-template-columns: repeat(3, 1fr);
+  justify-self: center;
+`;
+
+export const FilterWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  justify-items: stretch;
+  grid-column-gap: 25px;
 `;
