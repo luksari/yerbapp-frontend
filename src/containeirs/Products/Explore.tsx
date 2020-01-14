cb3db4dc9f2e2b5c6f265171f756aebd8e85efeb
luksari <luksari23@gmail.com>
2020-01-14 19:56:07 +0100

    Added delete for yerba

--- src/containeirs/Products/Explore.tsx
+++ src/containeirs/Products/Explore.tsx
@@ -3,13 +3,16 @@ import { Loader } from 'components/Loader';
 import { Title } from 'components/TitleBar';
 import { push } from 'connected-react-router';
 import { Wrapper } from 'containeirs/Users/styled';
-import { GetProductsDocument, GetProductsQuery, GetProductsQueryVariables } from 'generated/graphql';
+import {
+  GetProductsDocument, GetProductsQuery, GetProductsQueryVariables, useDeleteProductMutation,
+} from 'generated/graphql';
 import { usePagination } from 'hooks/usePagination';
 import React, { FC, memo } from 'react';
 import { useQuery } from 'react-apollo';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import { notificationError, notificationSuccess } from 'components/Notification';
 import { DataGrid } from './components/DataGrid';
 import { FilterForm } from './components/FilterForm';
 
@@ -30,6 +33,7 @@ const formValues = {
 
 const Explore: FC<Props> = memo(({
   redirectCreate,
+  redirectEdit,
 }) => {
   const { offset, perPage, setPage } = usePagination(4, 1);
 
@@ -43,6 +47,19 @@ const Explore: FC<Props> = memo(({
       fetchPolicy: 'network-only',
     },
   );
+
+  const [deleteProduct, { loading: deleting }] = useDeleteProductMutation({
+    onError: () => notificationError({ title: 'Wystąpił błąd', message: 'Nie udało się usunąć Yerba Mate!' }),
+    onCompleted: () => notificationSuccess({ title: 'Sukces', message: 'Pomyślnie usunięto Yerba Mate!' }),
+  });
+
+  const handleDelete = async (id: string) => {
+    try {
+      await deleteProduct({ variables: { productId: id } });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const handleSubmit = async (values: typeof formValues) => {
     const userId = localStorage.getItem('userId');
@@ -82,7 +99,12 @@ const Explore: FC<Props> = memo(({
           handleSubmit={handleSubmit}
         />
       </ActionBar>
-      <DataGrid data={data.products.items} isLoading={loading} />
+      <DataGrid
+        data={data.products.items}
+        isLoading={loading || deleting}
+        handleEdit={redirectEdit}
+        handleDelete={handleDelete}
+      />
     </Wrapper>
   );
 });
--- src/containeirs/Products/components/DataGrid.tsx
+++ src/containeirs/Products/components/DataGrid.tsx
@@ -7,11 +7,15 @@ import { CardGrid } from '../styled';
 interface Props {
   data: Partial<Product>[];
   isLoading?: boolean;
+  handleEdit: (id: string) => void;
+  handleDelete: (id: string) => void;
 }
 
 export const DataGrid: FC<Props> = memo(({
   data,
   isLoading,
+  handleDelete,
+  handleEdit,
 }) => {
   return (
     <CardGrid>
@@ -32,6 +36,8 @@ export const DataGrid: FC<Props> = memo(({
             priceScore={product.priceAverage}
             tasteScore={product.tasteAverage}
             photoUrl={product.photoUrl}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
           />
         ))}
     </CardGrid>
